80bb5ecc2c1a52397da27ee27ab8eef3588593ed
mansigupta10 <63924945+mansigupta10@users.noreply.github.com>
2020-05-02 19:46:43 -0400

    JavaScript additions

--- testscript.js
+++ testscript.js
@@ -1,7 +1,32 @@
 myitem = document.getElementById("firsttest");
 
 myitem.addEventListener("click", onClick);
+myitem.addEventListener("mouseout", onMouseOut);
 
 function onClick() {
     myitem.style.color = "green";
+    myitem.style.fontSize = "1.5em";
 }
+
+function onMouseOut() {
+    myitem.style.color = "";
+    myitem.style.fontSize = "";
+}
+
+thebutton = document.getElementById("thebutton");  
+otheritem = document.getElementById("buttontest");
+
+thebutton.addEventListener("click", onButtonClick);
+
+function onButtonClick() {  
+    otheritem.style.color = "red";  
+}
+
+textentry = document.getElementById("myinput");  
+textentry.addEventListener("change", onChange);
+
+function onChange() {  
+    newtext = myinput.value;  
+    otheritem.innerHTML = newtext;  
+}
+
